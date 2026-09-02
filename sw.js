6b4397c898902431963c7d9acce36dcc0c24d669
Surferyogi <koksum@yahoo.com>
2026-09-02 19:56:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * The cache name is stamped at build time by scripts/stamp-sw.js so a new build
  * invalidates the old cache instead of serving a stale bundle.
  */
-const CACHE_NAME = 'hoken-v2026-AUG-29-15-59'
+const CACHE_NAME = 'hoken-v2026-SEP-02-15-37'
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png']
 
 self.addEventListener('install', (event) => {
